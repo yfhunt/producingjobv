9fe0ddd73e87ac520a405022e6de877f7cea503f
yfhunt <yfhunt@163.com>
2021-12-17 11:26:59 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <video-frm></video-frm>
+</template>
+<script>
+import VideoFrm from './components/VideoFrm'
+export default {
+  name: 'App',
+  components: {
+    VideoFrm
+
+
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VideoFrm.vue
@@ -0,0 +1,11 @@
+<template>
+    <video controls>
+    </video>
+</template>
+<script>
+    export default {
+        name: "VideoFrm"
+    }
+</script>
+<style scoped>
+</style>
